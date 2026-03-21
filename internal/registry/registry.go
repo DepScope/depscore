65544346ed8efc7546884b660a7a997c3738f705
Jacob Verhoeks <jjverhoeks@schubergphilis.com>
2026-03-21 18:08:29 +0100

    feat: cached registry fetcher with disk persistence

--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -6,19 +6,19 @@ import (
 )
 
 type PackageInfo struct {
-	Name             string
-	Version          string
-	Ecosystem        string
-	TotalDownloads   int64
-	MonthlyDownloads int64
-	DownloadTrend    float64
-	LastReleaseAt    time.Time
-	FirstReleaseAt   time.Time
-	ReleaseCount     int
-	MaintainerCount  int
-	HasOrgBacking    bool
-	SourceRepoURL    string
-	IsDeprecated     bool
+	Name             string    `json:"name"`
+	Version          string    `json:"version"`
+	Ecosystem        string    `json:"ecosystem"`
+	TotalDownloads   int64     `json:"total_downloads"`
+	MonthlyDownloads int64     `json:"monthly_downloads"`
+	DownloadTrend    float64   `json:"download_trend"`
+	LastReleaseAt    time.Time `json:"last_release_at"`
+	FirstReleaseAt   time.Time `json:"first_release_at"`
+	ReleaseCount     int       `json:"release_count"`
+	MaintainerCount  int       `json:"maintainer_count"`
+	HasOrgBacking    bool      `json:"has_org_backing"`
+	SourceRepoURL    string    `json:"source_repo_url"`
+	IsDeprecated     bool      `json:"is_deprecated"`
 }
 
 type Fetcher interface {
